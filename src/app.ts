6a4ea407b79d4d6e2718f8a13f8aaf74797d7f96
Haroon Arshad <arshadharoon216@gmail.com>
2025-08-24 23:58:28 +0500

    Refactor app setup with improved middleware integration and logging

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,34 @@
-// import authRouter from "./routes/auth/auth.route";
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import errorMiddleware from './middlewares/error.middleware';
+import pino from 'pino';
+import config from './config';
+import pinoHttp from 'pino-http';
 
+const logger = pino({ level: config.LOG_LEVEL });
+const app = express();
 
+// request logger
+app.use(pinoHttp({ logger }));
 
-// app.use('/auth', authRouter)
+// build-in middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(cors({
+    origin: config.CLIENT_URL,
+    credentials: true,
+}));
+if (config.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
-// use morgan for api request logging 
+//404
+app.use((req, res) => res.status(404).json({ message: 'Route not found' }));
+
+// custom middlewares
+app.use(errorMiddleware);
+
+export default app;

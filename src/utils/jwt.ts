d0a739b7740af4cca3fd354f5c14da5f9a3b202a
Haroon Arshad <arshadharoon216@gmail.com>
2025-08-27 00:33:07 +0500

    Cast JWT expiration value to StringValue type for type safety

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,8 +1,9 @@
 import jwt from "jsonwebtoken";
 import config from "../config";
+import type { StringValue } from "ms";
 
 export function signToken(payload: Record<string, any>) {
-    return jwt.sign(payload, config.JWT_SECRET, { expiresIn: config.JWT_EXPIRES_IN });
+    return jwt.sign(payload, config.JWT_SECRET, { expiresIn: config.JWT_EXPIRES_IN as StringValue });
 }
 
 export function verifyToken<T = any>(token: string): T {

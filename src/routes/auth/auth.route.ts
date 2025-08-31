ce1311dce78fc7045723ccda4bbd0c96f6b33ec8
Haroon Arshad <arshadharoon216@gmail.com>
2025-08-31 21:10:57 +0500

    added route for getting me

--- src/routes/auth/auth.route.ts
+++ src/routes/auth/auth.route.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { uploadSingle } from "../../middlewares/multer.middleware";
 import { loginValidator, registerValidator } from "../../validators/auth/auth.validator";
-import { login, register } from "../../controllers/auth/auth.controller";
+import { getMe, login, register } from "../../controllers/auth/auth.controller";
+import authMiddleware from "../../middlewares/auth.middleware";
 
 const router = Router();
 
 router.post("/register", uploadSingle("avatar"), registerValidator, register);
 router.post("/login", loginValidator, login);
+router.get("/me", authMiddleware, getMe);
 
 export default router;

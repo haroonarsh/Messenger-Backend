e79e58948e2979b11595a9be8713b87bcbfe27e0
Haroon Arshad <arshadharoon216@gmail.com>
2025-08-31 21:09:56 +0500

    added code for store token in cookies

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -2,6 +2,14 @@ import { Request, Response, NextFunction } from "express";
 import { validationResult } from "express-validator";
 import * as AuthService from "../../services/auth/auth.service";
 
+const cookieOptions = () => ({
+    httpOnly: false,
+    secure: true,
+    sameSite: 'lax' as const, // lax
+    maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
+    path: '/'
+});
+
 export async function register(req: Request, res: Response, next: NextFunction) {
     try {
         // express-validator check
@@ -15,12 +23,16 @@ export async function register(req: Request, res: Response, next: NextFunction) 
             email: req.body.email,
             password: req.body.password,
             name: req.body.name,
+            bio: req.body.bio
         };
 
         // file from multer
         const file = (req as any).file as Express.Multer.File | undefined;
 
         const result = await AuthService.register(payload, file);
+
+        // store token in cookie
+        res.cookie('token', result.token, cookieOptions());
         return res.status(201).json(result);
     } catch (error) {
         next(error);
@@ -39,7 +51,17 @@ export async function login(req: Request, res: Response, next: NextFunction) {
             password: req.body.password,
         };
         const result = await AuthService.login(payload);
+
+        res.cookie('token', result.token, cookieOptions());
         return res.status(200).json(result);
+    } catch (error) {
+        next(error);
+    }
+}
+
+export async function getMe(req: Request, res: Response, next: NextFunction) {
+    try {
+        return res.status(200).json(req.user);
     } catch (error) {
         next(error);
     }

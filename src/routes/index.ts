0c5e47214233c26c6670114b5f3ab31e9e6cf7f1
Haroon Arshad <arshadharoon216@gmail.com>
2025-11-18 00:01:37 +0500

    add chat and user routes to main router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import authRoutes from "./auth/auth.route";
+import chatRoutes from "./chat/chat.routes";
+import userRoutes from "./user/user.routes";
 
 const router = Router();
 
 router.use("/user", authRoutes);
+router.use("/chat", chatRoutes);
+router.use("/user", userRoutes);
 
 export default router;

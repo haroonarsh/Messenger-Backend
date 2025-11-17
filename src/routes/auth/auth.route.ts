28eaca701a1b1742ee41e3137451686798d56596
Haroon Arshad <arshadharoon216@gmail.com>
2025-11-18 00:01:49 +0500

    add logout route to auth router

--- src/routes/auth/auth.route.ts
+++ src/routes/auth/auth.route.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { uploadSingle } from "../../middlewares/multer.middleware";
 import { loginValidator, registerValidator } from "../../validators/auth/auth.validator";
-import { getMe, login, register } from "../../controllers/auth/auth.controller";
+import { getMe, login, logout, register } from "../../controllers/auth/auth.controller";
 import authMiddleware from "../../middlewares/auth.middleware";
 
 const router = Router();
@@ -9,5 +9,6 @@ const router = Router();
 router.post("/register", uploadSingle("avatar"), registerValidator, register);
 router.post("/login", loginValidator, login);
 router.get("/me", authMiddleware, getMe);
+router.post("/logout", logout);
 
 export default router;

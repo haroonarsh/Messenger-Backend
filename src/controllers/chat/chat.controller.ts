0aa2f536b89019a36aa6fd613df2e197e0a42e27
Haroon Arshad <arshadharoon216@gmail.com>
2025-12-30 12:50:58 +0500

    refactor: streamline sendMessage and findOrCreateConversation logic

--- src/controllers/chat/chat.controller.ts
+++ src/controllers/chat/chat.controller.ts
@@ -22,10 +22,37 @@ export const getMessages = async (req: Request, res: Response) => {
 
 export const sendMessage = async (req: Request, res: Response) => {
     try {
-        const { conversationId, content, type, mediaUrl } = req.body;
-        const message = await ChatService.sendMessage(req.user?.id || '', conversationId, content, type, mediaUrl);
+        const { conversationId, text } = req.body;
+        console.log('userID:', req.user?.id);
+        
+        const message = await ChatService.sendMessage(
+            req.user?.id || '',
+            conversationId,
+            text,
+        );
         return res.status(200).json(message);
     } catch (error: any) {
         return res.status(500).json({ message: error.message });
+    }
+};
+
+export const findOrCreateConversation = async (req: Request, res: Response) => {
+    try {
+        const { participants } = req.body; // [userId1, userId2]
+        const currentUserId = req.user?.id;
+
+        if (!participants || !Array.isArray(participants) || participants.length !== 2) {
+            return res.status(400).json({ message: "Exactly two participants required" });
+        }
+
+        if (!participants.includes(currentUserId)) {
+        return res.status(403).json({ message: "Unauthorized" });
+        }
+
+        const result = await ChatService.findOrCreateConversation(participants);
+
+        return res.status(200).json(result);
+    } catch (error) {
+        return res.status(500).json({ message: (error as Error).message });
     }
 };

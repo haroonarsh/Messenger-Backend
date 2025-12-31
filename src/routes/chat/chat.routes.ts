11fd4958236e7cab6cc0478443972be90eab75eb
Haroon Arshad <arshadharoon216@gmail.com>
2025-12-31 10:34:23 +0500

    fixed some errors

--- src/routes/chat/chat.routes.ts
+++ src/routes/chat/chat.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import authMiddleware from "../../middlewares/auth.middleware";
-import { getConversations, getMessages, sendMessage } from "../../controllers/chat/chat.controller";
+import { findOrCreateConversation, getConversations, getMessages, sendMessage } from "../../controllers/chat/chat.controller";
 
 const router = Router();
 
 router.get("/conversations", authMiddleware, getConversations);
 router.get("/messages/:conversationId", authMiddleware, getMessages);
 router.post("/messages", authMiddleware, sendMessage);
+router.post("/conversations/find-or-create", authMiddleware, findOrCreateConversation);
 
 export default router;

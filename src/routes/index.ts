be8ee88b7fb123d3bfabbb43816a7acafe882043
Haroon Arshad <arshadharoon216@gmail.com>
2026-01-12 23:35:19 +0500

    add upload routes to main router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import authRoutes from "./auth/auth.route";
 import chatRoutes from "./chat/chat.routes";
 import userRoutes from "./user/user.routes";
+import uploadRoutes from "./upload/upload.routes";
 
 const router = Router();
 
 router.use("/user", authRoutes);
 router.use("/chat", chatRoutes);
 router.use("/user", userRoutes);
+router.use("/upload", uploadRoutes);
 
 export default router;

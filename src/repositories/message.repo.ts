63a3e322e4e99e7229fba939174e79bd229c330d
Haroon Arshad <arshadharoon216@gmail.com>
2026-01-12 23:34:59 +0500

    fix: make text field optional in IMessage interface and update createMessage function signature

--- src/repositories/message.repo.ts
+++ src/repositories/message.repo.ts
@@ -1,15 +1,16 @@
 import Message, { MessageDocument } from "../models/message.model";
 
-export const createMessage = async (data: { conversationId: string, sender: object, text: string }): Promise<any> => {
+export const createMessage = async (data: { conversationId: string, sender: object, text: string; type?: "text" | "image" | "video"; mediaUrl?: string }): Promise<any> => {
     const message = new Message(data);
     await message.save();
     return message;
 };
 
-export const getMessagesByConversation = async (conversationId: string, limit: number = 50): Promise<MessageDocument[]> => {
+export const getMessagesByConversation = async (conversationId: string, skip: number = 0, limit: number = 30): Promise<MessageDocument[]> => {
     return await Message.find({ conversationId })
-        .populate("sender", "username avatar")
+        .populate("sender", "name username avatar")
         .sort({ createdAt: -1 })
+        .skip(skip)
         .limit(limit)
         .exec();
 };

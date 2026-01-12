bb33b6b0c64ae0951f14ee3171ed4cb555416189
Haroon Arshad <arshadharoon216@gmail.com>
2026-01-12 23:33:41 +0500

    feat: implement media upload functionality with Cloudinary integration

---
+++ src/config/multer.ts
@@ -0,0 +1,21 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.memoryStorage(); // Kepp in moemory, we'll upload to cloudinary
+
+const upload = multer({
+    storage,
+    limits: { fileSize: 50 * 1024 * 1024 }, // Max file size: 50MB
+    fileFilter: (req, file, cb) => {
+        const allowedTypes = /jpeg|jpg|png|gif|webp|heic|bmp|tiff|mp4|mov|avi|webm|mkv|3gp|wmv|flv/;
+        const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
+        const mimetype = allowedTypes.test(file.mimetype.toLowerCase());
+
+        if (extname && mimetype) {
+            return cb(null, true);
+        }
+        cb(new Error("Invalid file type. Only images and videos allowed"));
+    },
+});
+
+export default upload;
